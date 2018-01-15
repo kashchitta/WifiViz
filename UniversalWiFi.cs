d6c6a43bcc931477ff20f6cff249d5424f4e7b3c
Nikhil Chand Kashyap Chitta <kash.chitta@gmail.com>
2018-01-15 18:40:08 -0500

    Add files via upload

---
+++ UniversalWiFi.cs
@@ -0,0 +1,83 @@
+﻿using Assets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+#if !UNITY_EDITOR && UNITY_METRO
+using Windows.Devices.WiFi;
+#endif
+
+namespace Assets
+{
+#if !UNITY_EDITOR && UNITY_METRO
+    public class UniversalWiFi : IWiFiAdapter
+    {
+        private bool IsReady { get; set; }
+        private uint Signal { get; set; }
+        private string Report { get; set; }
+        private string Ssid { get; set; }
+        public UniversalWiFi()
+        {
+            IsReady = true;
+            Signal = 0u;
+            Ssid = string.Empty;
+            Report = string.Empty;
+        }
+        public uint GetSignal(string ssid)
+        {
+            Ssid = ssid;
+            Scan();
+            return Signal;
+        }
+
+        private async void Scan()
+        {
+            if (IsReady)
+            {
+                IsReady = false;
+                uint signal = 0;
+                var result = await WiFiAdapter.FindAllAdaptersAsync();
+                if (result.Count >= 1)
+                {
+                    var firstAdapter = result[0];
+                    await firstAdapter.ScanAsync();
+                    GenerateNetworkReport(firstAdapter.NetworkReport);
+                    if (!string.IsNullOrEmpty(Ssid))
+                    {
+                        signal = GetNetworkSignal(firstAdapter.NetworkReport, Ssid);
+                    }
+                }
+                IsReady = true;
+                Signal = signal;
+            }
+        }
+
+        private byte GetNetworkSignal(WiFiNetworkReport report, string ssid)
+        {
+            var network = report.AvailableNetworks.Where(x => x.Ssid.ToLower() == ssid.ToLower()).FirstOrDefault();
+            return network.SignalBars;
+        }
+
+        private void GenerateNetworkReport(WiFiNetworkReport report)
+        {
+            var networks = new List<string>();
+            foreach (var network in report.AvailableNetworks)
+            {
+                //networks.Add(string.Format("SSID: {0} -- SignalBars: {1} -- Db: {2} -- Mac: {3}",
+                networks.Add(string.Format("SSID: {0} -- SignalBars: {1} -- Db: {2}",
+//network.Ssid, network.SignalBars, network.NetworkRssiInDecibelMilliwatts, network.Bssid));
+network.Ssid, network.SignalBars, network.NetworkRssiInDecibelMilliwatts));
+            }
+
+            Report = string.Join(Environment.NewLine, networks.ToArray());
+        }
+
+        public string GetNetworkReport()
+        {
+            Scan();
+            return Report;
+        }
+    }
+#endif
+}
---
+++ WiFiScanner.cs
@@ -0,0 +1,38 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Net.NetworkInformation;
+using System;
+using Assets;
+ 
+public class WiFiScanner : MonoBehaviour
+{
+ 
+    public Text Display;    // Text UI to display the network information.
+#if !UNITY_EDITOR && UNITY_METRO
+    private IWiFiAdapter WiFiAdapter;
+    // Use this for initialization
+    void Start()
+    {
+        WiFiAdapter = new UniversalWiFi();
+    }
+ 
+    // This method is being called every frame.
+    void Update()
+    {
+        Scan();
+    }
+    private void Scan()
+    {
+        if (WiFiAdapter != null)
+        {
+            var ssid = "RMSLab"; // The desired SSID to scan.
+            var signal = WiFiAdapter.GetSignal(ssid);
+            var report = WiFiAdapter.GetNetworkReport();
+            Debug.Log(string.Format("{0} signal:{1}{2}", ssid, signal, report));
+            Display.text = string.Format("{0} signal:{1}{2}", ssid, signal, report);
+        }
+    }
+#endif
+}
